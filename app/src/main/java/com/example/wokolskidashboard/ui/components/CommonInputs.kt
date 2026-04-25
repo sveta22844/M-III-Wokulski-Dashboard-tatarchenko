c07cfec50d82e4eb482e9e9d24ffa9f0ba329e0d
sveta22844 <tatarchenkosveta24@gmail.com>
2026-04-25 19:06:38 +0200

    zrobione wspólne komponenty żeby UI (elementy interfejsu) były taki same

--- app/src/main/java/com/example/wokolskidashboard/ui/components/CommonInputs.kt
+++ app/src/main/java/com/example/wokolskidashboard/ui/components/CommonInputs.kt
@@ -1,2 +1,28 @@
-package com.example.wokolskidashboard.ui.components
+package com.example.wokulskidashboard.ui.components
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
 
+
+
+@Composable
+fun WokulskiTextField(
+    value: String,
+    onChange: (String) -> Unit,
+    label: String
+){
+    TextField(
+        value = value,
+        onValueChange = onChange,
+        label = {Text(label) }
+    )
+}
+
+@Composable
+fun WokulskiButton(
+    text: String,
+    onClick: () -> Unit
+) {
+    Button(onClick = onClick){
+        Text(text)
+    }
+}
