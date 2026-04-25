ef5201018eab7e8f976ec44ededfd6091afedba9
sveta22844 <tatarchenkosveta24@gmail.com>
2026-04-25 19:33:58 +0200

    zrobiony komponent (poki co bez stanu)

--- app/src/main/java/com/example/wokolskidashboard/ui/components/BalanceHeader.kt
+++ app/src/main/java/com/example/wokolskidashboard/ui/components/BalanceHeader.kt
@@ -1,1 +1,12 @@
-package com.example.wokolskidashboard.ui.components
+package com.example.wokulskidashboard.ui.components
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+
+@Composable
+fun BalanceHeader(balance: Double) {
+    //wyświetlanie tekstu z saldem
+    Text(
+        text = "Saldo: %.2f zł".format(balance),
+        style = MaterialTheme.typography.headlineMedium
+    )
+}
