cd5134facf451c81446084e93dfbd777341591ec
sveta22844 <tatarchenkosveta24@gmail.com>
2026-04-25 20:11:10 +0200

    zbudowanie UI, zrobiony główny stan (state hoisting) podłączone wyswietlenie

--- app/src/main/java/com/example/wokolskidashboard/ui/MainScreen.kt
+++ app/src/main/java/com/example/wokolskidashboard/ui/MainScreen.kt
@@ -1,2 +1,26 @@
-package com.example.wokolskidashboard.ui
+package com.example.wokulskidashboard.ui
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.wokolskidashboard.model.Transaction
+import com.example.wokulskidashboard.ui.components.BalanceHeader
+
+@Composable
+fun MainScreen() { //buduje UI w Compose
+
+    //główny stan/lista transakcji
+    val transactions = remember { mutableStateListOf<Transaction>() }
+
+    //liczenie salda na podstawie transakcjii
+    val balance = transactions.sumOf {  //sprawdzanie czy to wydatek
+        if (it.isExpense) -it.kwota else it.kwota //if wydatek -kwota alse +kwote
+    }
+
+    Column(modifier = Modifier.padding(16.dp)) {
+        BalanceHeader(balance) //wyświetlenie przez komponent dla nagłowka
+    }
+}
