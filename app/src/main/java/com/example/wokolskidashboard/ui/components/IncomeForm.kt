988bb8b53ae1447e18d36a5cb1fb414905343b7a
sveta22844 <tatarchenkosveta24@gmail.com>
2026-04-25 21:03:36 +0200

    gotowy formularz Rzeckiego, wysyla dane do rodzica do MainScreen, resetuje pola formularza po dodaniu danych

--- app/src/main/java/com/example/wokolskidashboard/ui/components/IncomeForm.kt
+++ app/src/main/java/com/example/wokolskidashboard/ui/components/IncomeForm.kt
@@ -1,2 +1,45 @@
 package com.example.wokolskidashboard.ui.components
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import com.example.wokulskidashboard.ui.components.WokulskiButton
+import com.example.wokulskidashboard.ui.components.WokulskiTextField
+
+
+@Composable
+fun IncomeForm(
+    onAddIncome: (String, Double) -> Unit //funkcjia przekazania z gory/przekazuje nazwe i kwote do rodzica
+){
+    //stany pól
+    var nazwa by remember { mutableStateOf("") }
+    var kwota by remember { mutableStateOf("") }
+
+    Column {
+        Text("Przychód")
+
+        //aktualizacja nazwy i kwoty
+        WokulskiTextField(nazwa, { nazwa = it }, "Nazwa")
+        WokulskiTextField(kwota, { kwota = it }, "Kwota")
+
+        WokulskiButton("Dodaj") { //wykonuje logikę po kliknięciu
+
+            val value = kwota.toDoubleOrNull()  //próba zamiany tekstu na liczbę/nie uda -> null
+
+            // walidacja
+            if (nazwa.isNotBlank() && value != null && value > 0) {
+                //czy nazwa nie jest pusta, czy jest poprawna,czy wartość > 0,
+
+                onAddIncome(nazwa, value) //wysyla dane do rodzica(state hoisting)
+
+                nazwa = ""
+                kwota = ""
+                //resetuje formularz po dodaniu danych
+            }
+        }
+    }
+}
